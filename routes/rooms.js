fd9284933a6c51178858907d9df0c02f89b459b9
Hatsxne <malnu061@uottawa.ca>
2024-03-30 20:41:45 -0400

    Updated rooms.js - Maisam

--- routes/rooms.js
+++ routes/rooms.js
@@ -4,6 +4,5 @@ const roomsController = require('../controllers/roomsController');
 
 router.get('/search', roomsController.searchAvailableRooms);
 router.delete('/:roomId', roomsController.deleteRoom);
-router.get('/search', roomsController.searchRooms);
 
 module.exports = router;
